3c6df94ef499ab7cf767ce6c3442b9d03047f5e5
Faruk Cesur <farukces110@gmail.com>
2021-08-05 17:15:53 +0300

    Creating Round System (Bricks Spawns Each Round)

--- Assets/Game/Scripts/BallController.cs
+++ Assets/Game/Scripts/BallController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -5,30 +6,57 @@ public class BallController : MonoBehaviour
 {
     public PlayerController playerController;
     public BrickSpawner brickSpawner;
-    private bool isStarted;
-    private Rigidbody2D rb;
+    private bool _isStarted;
+    private Rigidbody2D _rb;
+    private int _collisionMeter;
     [HideInInspector] public Vector3 ballStartPos;
 
 
     void Start()
     {
         ballStartPos = transform.position;
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
         gameObject.transform.SetParent(playerController.gameObject.transform);
     }
 
     private void Update()
     {
-        if (isStarted == false)
+        if (_isStarted == false)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 gameObject.transform.SetParent(null);
-                rb.constraints = RigidbodyConstraints2D.None;
-                rb.AddForce(Vector3.up * 300f);
-                isStarted = true;
+                _rb.constraints = RigidbodyConstraints2D.None;
+                _rb.AddForce(Vector3.up * 300f);
+                _isStarted = true;
             }
         }
+    }
+
+    private void MoveDownSpawnBricks()
+    {
+        foreach (var brick in brickSpawner.spawnedBricks)
+        {
+            brick.transform.position -= new Vector3(0, 0.5f, 0);
+        }
+
+        brickSpawner.brickPos = brickSpawner.tempBrickPos;
+
+        for (int i = 0; i < 5; i++)
+        {
+            brickSpawner.randomBrick = Random.Range(0, 6);
+            brickSpawner.spawnedBrick =
+                Instantiate(brickSpawner.bricks[brickSpawner.randomBrick],
+                    brickSpawner.brickPos, Quaternion.identity, brickSpawner.parentBrick.transform);
+            brickSpawner.brickPos = brickSpawner.brickPos + new Vector3(1, 0, 0);
+            brickSpawner.spawnedBricks.Add(brickSpawner.spawnedBrick);
+        }
+
+        transform.position = ballStartPos;
+        playerController.gameObject.transform.position = new Vector3(0, -4, 0);
+        gameObject.transform.SetParent(playerController.gameObject.transform);
+        _rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        _isStarted = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -37,28 +65,21 @@ public class BallController : MonoBehaviour
 
         if (deathArea)
         {
-            foreach (var brick in brickSpawner.spawnedBricks)
+            MoveDownSpawnBricks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerController player = other.gameObject.GetComponentInParent<PlayerController>();
+
+        if (player)
+        {
+            _collisionMeter++;
+            if (_collisionMeter % 25 == 0)
             {
-                brick.transform.position -= new Vector3(0, 0.5f, 0);
+                MoveDownSpawnBricks();
             }
-
-            brickSpawner.brickPos = brickSpawner.tempBrickPos;
-
-            for (int i = 0; i < 5; i++)
-            {
-                brickSpawner.randomBrick = Random.Range(0, 6);
-                brickSpawner.spawnedBrick =
-                    Instantiate(brickSpawner.bricks[brickSpawner.randomBrick],
-                        brickSpawner.brickPos, Quaternion.identity, brickSpawner.parentBrick.transform);
-                brickSpawner.brickPos = brickSpawner.brickPos + new Vector3(1, 0, 0);
-                brickSpawner.spawnedBricks.Add(brickSpawner.spawnedBrick);
-            }
-
-            transform.position = ballStartPos;
-            playerController.gameObject.transform.position = new Vector3(0, -4, 0);
-            gameObject.transform.SetParent(playerController.gameObject.transform);
-            rb.constraints = RigidbodyConstraints2D.FreezeAll;
-            isStarted = false;
         }
     }
 }
--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -17,4 +17,9 @@ public class GameManager : MonoBehaviour
         {
         }
     }
+
+    public void Retry()
+    {
+        
+    }
 }
